9cbcfb97b74909c790ea259fb2fecae19cdbbb05
suguoxu <1573591859@qq.com>
2022-11-23 22:06:41 +0800

    补充部分uboot启动流程内容

--- uboot/启动流程(ARM_v7)/0、soc启动流程.c
+++ uboot/启动流程(ARM_v7)/0、soc启动流程.c
@@ -12,9 +12,9 @@ FLASH(eMMC/NAND)中的SPL加载到IRAM, 然后直接跳转到SPL运行, 然后�
 ROMCODE中的固化程序。ROMCODE会通过检测启动方式来加载第二阶段bootloader(SPL)。
     uboot已经是一个bootloader了, 那么为什么还多一个uboot SPL呢？主要原因是对于一些SOC来说, 它的内部SRAM可能会比较小, 小到无法装载下一个完整的uboot镜像, 那么就需要SPL, 它主要
 负责初始化外部RAM和环境, 并加载真正的uboot镜像到外部RAM中来执行。所以由此来看, SPL是一个非常小的loader程序, 可以运行于SOC的内部SRAM中, 它的主要功能就是加载真正的uboot并运行之。
-    在 U-Boot 源码中, 启动过程没有完全单独出 SPL 的代码, 而是复用了大量 U-Boot 里面的代码。在代码中, 通过宏 CONFIG_SPL_XXX 来进行区分。因此, SPL 的启动 与 U-Boot 的启动流程
-是一样的。至于SPL的入口在哪，可以查看spl的链接脚本（./arch/arm/cpu/u-boot-spl.lds），在链接脚本中，可以看到入口地址为 _start，这部分和uboot一致，对应于路径 
-u-boot/arch/arm/lib/vector.S 中的 _start。
+    在 U-Boot 源码中, 没有完全单独出 SPL 的代码, 而是复用了大量 U-Boot 里面的代码。在代码中, 通过宏 CONFIG_SPL_XXX 来进行区分。因此, SPL 的启动 与 U-Boot 的启动流程一样的。至
+于SPL的入口在哪，可以查看spl的链接脚本（./arch/arm/cpu/u-boot-spl.lds），在链接脚本中，可以看到入口地址为 _start，这部分和uboot一致，对应于路径 u-boot/arch/arm/lib/vector.S 中
+的 _start。
     CPU刚上电, 需要小心的设置好很多状态, 包括cpu状态、中断状态、MMU状态等等。在armv7架构的uboot-spl, 主要完成以下功能：
     (1)完成arch相关的初始化; 
         关闭中断, svc模式
@@ -45,7 +45,12 @@ SPL编译：
     board_init_f()
     board_init_r()
     main_loop()
-
+	
+	函数 board_init_f_alloc_reserve 主要是留出早期的 malloc 内存区域和 gd 内存区域
+	函数 board_init_f_init_reserve 用于初始化 gd，其实就是清零处理。并且还设置了gd->malloc_base 为 gd 基地址+gd 大小，再做 16 字节对齐
+	函数 relocate_code 是代码重定位函数，此函数负责将 uboot 拷贝到新的地方去
+	函数 relocate_vectors 对中断向量表做重定位
+	
 4、linux kernel 阶段
 
 5、File system 阶段
