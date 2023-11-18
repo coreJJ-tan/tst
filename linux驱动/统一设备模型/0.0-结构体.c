6b339eb5cc91093a01ec8b6e12c5964d27969868
suguoxu <1573591859@qq.com>
2023-11-18 21:14:44 +0800

    添加统一设备模型 core.c OK

--- linux驱动/统一设备模型/0.0-结构体.c
+++ linux驱动/统一设备模型/0.0-结构体.c
@@ -75,7 +75,8 @@ struct device {
     struct device       *parent; // 该设备的父设备, 一般是该设备所从属的bus、controller等设备.
     struct device_private   *p; // 一个用于struct device的私有数据结构指针, 该指针中会保存子设备链表、用于添加到bus/driver/prent等设备中的链表头等等.
     struct kobject kobj;
-    const char      *init_name; //该设备的名称 /* initial name of the device */ 
+    const char      *init_name; //该设备的默认名称, 只在device_add()刚添加设备时会起作用, 可以使用 dev_set_name()设置设备名字,sysfs下设备的名字其实就是它的 kobject 的名字,最终设置的是kobject的名字
+                                // 在device_add()之后,该成员会指向NULL
     const struct device_type *type;
     struct mutex        mutex;  /* mutex to synchronize calls to its driver. */
     struct bus_type *bus;       // 该device属于哪个总线/* type of bus device is on */
@@ -112,5 +113,182 @@ struct device {
     void    (*release)(struct device *dev); // device_release()会使用到, 如果定义, 对应的kobject引用计数为0时会调用
     struct iommu_group  *iommu_group;
     bool            offline_disabled:1;
-    bool            offline:1;
+    bool            offline:1;  // true 表示设备处于 offline 状态
+};
+
+/*
+ * The type of device, "struct device" is embedded in. A class
+ * or bus can contain devices of different types
+ * like "partitions" and "disks", "mouse" and "event".
+ * This identifies the device type and carries type-specific
+ * information, equivalent to the kobj_type of a kobject.
+ * If "name" is specified, the uevent will contain it in
+ * the DEVTYPE variable.
+ */
+struct device_type {
+    const char *name;
+    const struct attribute_group **groups;
+    int (*uevent)(struct device *dev, struct kobj_uevent_env *env);
+    char *(*devnode)(struct device *dev, umode_t *mode,
+             kuid_t *uid, kgid_t *gid);
+    void (*release)(struct device *dev);
+
+    const struct dev_pm_ops *pm;
+};
+
+/**
+ * struct class - device classes
+ * @name:	Name of the class.
+ * @owner:	The module owner.
+ * @class_attrs: Default attributes of this class.
+ * @dev_groups:	Default attributes of the devices that belong to the class.
+ * @dev_kobj:	The kobject that represents this class and links it into the hierarchy.
+ * @dev_uevent:	Called when a device is added, removed from this class, or a
+ *		few other things that generate uevents to add the environment
+ *		variables.
+ * @devnode:	Callback to provide the devtmpfs.
+ * @class_release: Called to release this class.
+ * @dev_release: Called to release the device.
+ * @suspend:	Used to put the device to sleep mode, usually to a low power
+ *		state.
+ * @resume:	Used to bring the device from the sleep mode.
+ * @ns_type:	Callbacks so sysfs can detemine namespaces.
+ * @namespace:	Namespace of the device belongs to this class.
+ * @pm:		The default device power management operations of this class.
+ * @p:		The private data of the driver core, no one other than the
+ *		driver core can touch this.
+ *
+ * A class is a higher-level view of a device that abstracts out low-level
+ * implementation details. Drivers may see a SCSI disk or an ATA disk, but,
+ * at the class level, they are all simply disks. Classes allow user space
+ * to work with devices based on what they do, rather than how they are
+ * connected or how they work.
+ */
+struct class {
+	const char		*name;
+	struct module		*owner;
+
+	struct class_attribute		*class_attrs;
+	const struct attribute_group	**dev_groups;
+	struct kobject			*dev_kobj;
+
+	int (*dev_uevent)(struct device *dev, struct kobj_uevent_env *env);
+	char *(*devnode)(struct device *dev, umode_t *mode);
+
+	void (*class_release)(struct class *class);
+	void (*dev_release)(struct device *dev);
+
+	int (*suspend)(struct device *dev, pm_message_t state);
+	int (*resume)(struct device *dev);
+
+	const struct kobj_ns_type_operations *ns_type;
+	const void *(*namespace)(struct device *dev);
+
+	const struct dev_pm_ops *pm;
+
+	struct subsys_private *p;
+};
+/**
+ * struct device_private - structure to hold the private to the driver core portions of the device structure.
+ *
+ * @klist_children - klist containing all children of this device
+ * @knode_parent - node in sibling list
+ * @knode_driver - node in driver list
+ * @knode_bus - node in bus list
+ * @deferred_probe - entry in deferred_probe_list which is used to retry the
+ *  binding of drivers which were unable to get all the resources needed by
+ *  the device; typically because it depends on another driver getting
+ *  probed first.
+ * @device - pointer back to the struct class that this structure is
+ * associated with.
+ *
+ * Nothing outside of the driver core should ever touch these fields.
+ */
+struct device_private {
+    struct klist klist_children;
+    struct klist_node knode_parent;
+    struct klist_node knode_driver;
+    struct klist_node knode_bus;
+    struct list_head deferred_probe;
+    struct device *device;
+};
+
+/**
+ * struct bus_type - The bus type of the device
+ *
+ * @name:	The name of the bus.
+ * @dev_name:	Used for subsystems to enumerate devices like ("foo%u", dev->id).
+ * @dev_root:	Default device to use as the parent.
+ * @dev_attrs:	Default attributes of the devices on the bus.
+ * @bus_groups:	Default attributes of the bus.
+ * @dev_groups:	Default attributes of the devices on the bus.
+ * @drv_groups: Default attributes of the device drivers on the bus.
+ * @match:	Called, perhaps multiple times, whenever a new device or driver
+ *		is added for this bus. It should return a nonzero value if the
+ *		given device can be handled by the given driver.
+ * @uevent:	Called when a device is added, removed, or a few other things
+ *		that generate uevents to add the environment variables.
+ * @probe:	Called when a new device or driver add to this bus, and callback
+ *		the specific driver's probe to initial the matched device.
+ * @remove:	Called when a device removed from this bus.
+ * @shutdown:	Called at shut-down time to quiesce the device.
+ *
+ * @online:	Called to put the device back online (after offlining it).
+ * @offline:	Called to put the device offline for hot-removal. May fail.
+ *
+ * @suspend:	Called when a device on this bus wants to go to sleep mode.
+ * @resume:	Called to bring a device on this bus out of sleep mode.
+ * @pm:		Power management operations of this bus, callback the specific
+ *		device driver's pm-ops.
+ * @iommu_ops:  IOMMU specific operations for this bus, used to attach IOMMU
+ *              driver implementations to a bus and allow the driver to do
+ *              bus-specific setup
+ * @p:		The private data of the driver core, only the driver core can
+ *		touch this.
+ * @lock_key:	Lock class key for use by the lock validator
+ *
+ * A bus is a channel between the processor and one or more devices. For the
+ * purposes of the device model, all devices are connected via a bus, even if
+ * it is an internal, virtual, "platform" bus. Buses can plug into each other.
+ * A USB controller is usually a PCI device, for example. The device model
+ * represents the actual connections between buses and the devices they control.
+ * A bus is represented by the bus_type structure. It contains the name, the
+ * default attributes, the bus' methods, PM operations, and the driver core's
+ * private data.
+ */
+struct bus_type {
+	const char		*name;
+	const char		*dev_name;
+	struct device		*dev_root;
+	struct device_attribute	*dev_attrs;	/* use dev_groups instead */
+	const struct attribute_group **bus_groups;
+	const struct attribute_group **dev_groups;
+	const struct attribute_group **drv_groups;
+
+	int (*match)(struct device *dev, struct device_driver *drv);
+	int (*uevent)(struct device *dev, struct kobj_uevent_env *env);
+	int (*probe)(struct device *dev);
+	int (*remove)(struct device *dev);
+	void (*shutdown)(struct device *dev);
+
+	int (*online)(struct device *dev);
+	int (*offline)(struct device *dev);
+
+	int (*suspend)(struct device *dev, pm_message_t state);
+	int (*resume)(struct device *dev);
+
+	const struct dev_pm_ops *pm;
+
+	const struct iommu_ops *iommu_ops;
+
+	struct subsys_private *p;
+	struct lock_class_key lock_key;
+};
+
+struct subsys_interface {
+	const char *name;
+	struct bus_type *subsys;
+	struct list_head node;
+	int (*add_dev)(struct device *dev, struct subsys_interface *sif);
+	int (*remove_dev)(struct device *dev, struct subsys_interface *sif);
 };
--- linux驱动/统一设备模型/函数接口/core.c
+++ linux驱动/统一设备模型/函数接口/core.c
@@ -1,3 +1,10 @@
+0. 初始化阶段
+    int __init devices_init(void)
+        创建一个 kset, 名为 devices_kset, 对应 sysfs 下的 "/sys/devices" 目录
+        创建一个 kobject, 名为 dev_kobj, 对应 sysfs 下的 "/sys/dev" 目录
+        创建一个 kobject, 名为 sysfs_dev_block_kobj, 对应 sysfs 下的 "/sys/dev/block" 目录
+        创建一个 kobject, 名为 sysfs_dev_char_kobj, 对应 sysfs 下的 "/sys/dev/char" 目录
+
 1. const char *dev_driver_string(const struct device *dev)
     获取设备绑定的驱动的名字
     dev: 要获取的设备结构体
@@ -85,7 +92,7 @@
     attr: 要创建的属性文件的上层 device_attribute 实例
 
 13. void device_remove_file(struct device *dev, const struct device_attribute *attr)
-    在该设备的目录移除属性文件。
+    在该设备的目录移除属性文件.
     dev: 要移除的属性文件的设备目录
     attr: 要移除的属性文件的上层 device_attribute 实例
 
@@ -93,10 +100,103 @@
 
 15. void device_remove_bin_file(struct device *dev, const struct bin_attribute *attr)
 
-16. void device_initialize(struct device *dev)
+16. struct kobject *virtual_device_parent(struct device *dev)
+    在 /sys/devices/ 下创建一个 kobject, 名为 "virtual"
+    dev: 未使用该参数
+
+    返回值: 返回该 virtual 对应的 kobject, 这是个静态局部变量
+
+    * 这个目录下的设备都有什么含义呢？
+    
+17. int dev_set_name(struct device *dev, const char *fmt, ...)
+    设置 device 在 sysfs 下的目录名
+    dev: 要设置目录名的设备.
+    fmt: 名字
+
+    返回值: 
+
+18. void device_initialize(struct device *dev)
     初始化一个 device, 主要是:
     (1) 指定 device->kobj 的 kset, 该 kset 对应的目录是 /sys/devices/
     (2) 指定 device->kobj 的 ktype 为 device_ktype
     (3) 初始化各种锁, 链表头, 电源管理相关的
 
-17. struct kobject *virtual_device_parent(struct device *dev)
+19. int device_add(struct device *dev)
+    向内核添加一个初始化过的 device, 主要是
+    (1) 增加 device 的引用计数, 其实就是增加 device->kobj 的引用计数, 表示当前 device 的目录被引用了一次 (当引用计数减小到0的时候, 会触发一连串的 ->release() 回调, 释放各种内存)
+    (2) 申请该设备的私有结构, 即 device->p 成员 (struct device_private 类型)
+    (3) 设置该设备的名字: 可以在 device_add()之前使用 dev_set_name()设置名字,或者使用 dev->init_name 设置名字, 二者中 dev->init_name 优先, 如果两者都未实现, 会使用该设备所属的 bus 名加上一个
+        index 命名, 如果还没有所属的 bus, 那么 device_add() 将会失败
+    (4) 选择该 device 的目录创建于哪个 sysfs 目录下, 稍后会指定 device 的 kobject 的父 kobject 为该目录的 kobject// 这里面门道较多，以后再总结
+    (5) 调用 kobject_add() 添加 device 的 kobject 到内核并创建对应的目录
+    (6) 在创建好的目录下创建 uevent 属性文件 // 关于该文件的读写，以后再研究
+    (7) 如果该 device 对应一个设备树节点, 那么在该 device 目录下创建一个软链接, 指向该设备树节点对应的 sysfs 下的目录, 软链接名为 "of_node"
+    (8) 如果该 device 属于某个 class:
+        在该 device 目录下创建一个软链接, 指向 class->p->subsys.kobj 对应的目录, 软链接名为 "subsystem"
+        对于非 partition 设备, 在该 device 目录下创建一个软链接, 指向父设备对应的目录, 软链接名为 "device"
+        在该 device->class->p->subsys.kobj 目录下创建一个软链接, 指向 device 对应的目录, 软链接名为该 device 的名字
+    (9) // 调用 device_add_attrs() 创建一些列的文件, 以后再研究吧
+    (10) // 调用 bus_add_device() 创建一些列的文件, 以后再研究吧
+        klist_add_tail(&dev->p->knode_bus, &bus->p->klist_devices);
+    (11) // 调用 dpm_sysfs_add() device_pm_add() , 以后再研究吧
+    (12) 如果该函数调用之前指定了设备号(即设备号不为 0):
+            在该设备的目录下创建 "dev" 属性文件, 可通过该文件查看设备号
+            在 "/sys/dev/char/" 目录下创建软链接，指向当前 device 的目录, 软链接名字格式为 "主设备号:子设备号"
+            创建该设备在 /dev 目录下的文件节点 // devtmpfs_create_node(dev);
+    (13) // 如果设备有 bus, 调用 blocking_notifier_call_chain(&dev->bus->p->bus_notifier, BUS_NOTIFY_ADD_DEVICE, dev);
+    (14) // 调用 kobject_uevent(&dev->kobj, KOBJ_ADD);
+    (15) // 调用 bus_probe_device(dev);
+    (16) 将该设备加入父设备为头的 klist 链表中, 也将该设备加入设备所属的类为头的 klist 链表中
+    (17) 通知类中的其他设备, 有设备进行添加操作?
+    (17) 减小 device 的引用计数
+
+20. void device_del(struct device *dev)
+
+21. int device_register(struct device *dev)
+        device_initialize(dev);
+        return device_add(dev);
+
+22. void device_unregister(struct device *dev)
+
+23. struct device *get_device(struct device *dev)
+    增加该设备的引用计数, 本质是增加该设备对应的 kobject 的引用计数
+
+24. void put_device(struct device *dev)
+    减少该设备的引用计数, 本质是减少该设备对应的 kobject 的引用计数
+
+25. const char *device_get_devnode(struct device *dev, umode_t *mode, kuid_t *uid, kgid_t *gid, const char **tmp)
+
+26. int device_for_each_child(struct device *parent, void *data, int (*fn)(struct device *dev, void *data))
+    遍历 device 下的所有子设备, 对每个子设备调用 fn() 函数, 只要一个 fn() 执行失败, 那么就返回了不再执行. 其中 data 是 fn() 函数的一个形参
+
+27. struct device *device_find_child(struct device *parent, void *data, int (*match)(struct device *dev, void *data))
+    对 device 下的所有子设备, 执行 match() 函数进行匹配, 匹配到了则返回对应子设备的实例
+
+28. int device_offline(struct device *dev)
+
+29. int device_online(struct device *dev)
+
+20 static inline bool device_supports_offline(struct device *dev)
+    return dev->bus && dev->bus->offline && dev->bus->online;
+
+31. struct device *__root_device_register(const char *name, struct module *owner)
+    #define root_device_register(name) __root_device_register(name, THIS_MODULE)
+
+32. void root_device_unregister(struct device *dev)
+
+33. struct device *device_create(struct class *class, struct device *parent, dev_t devt, void *drvdata, const char *fmt, ...)
+    申请一个 device, 并调用 device_initialize() 进行初始化, 随后用形参填充部分成员, 最后调用 device_add() 向内核添加该设备
+
+    * device_register() 和 device_create() 的区别就是: 调用 device_register() 之前, 先创建 device 实例并初始化部分成成员, 最后添加到内核, 而 device_create() 直接内部创建并初始化, 最后添加到内核
+
+34. struct device *device_create_with_groups(struct class *class, struct device *parent, dev_t devt, void *drvdata, const struct attribute_group **groups, const char *fmt, ...)
+    和 device_create() 的区别是, 在创建 device 的时候还会创建 groups 中指定的属性文件,其余作用一致
+
+35. void device_destroy(struct class *class, dev_t devt)
+    device_register() 和 device_create() 的相反作用
+
+36. int device_rename(struct device *dev, const char *new_name)
+
+37. int device_move(struct device *dev, struct device *new_parent, enum dpm_order dpm_order)
+
+38. void device_shutdown(void)
